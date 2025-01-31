e330309fcec08883b575ed69fb871c6aa264232a
Robert Schmidt <robert.schmidt@myvision.software>
2025-01-31 10:01:47 +0100

    examples and getstarted updated

--- examples/example7/example7.js
+++ examples/example7/example7.js
@@ -6,10 +6,10 @@ const example7 = new RobJSApp('example7');
 example7.init('example7')
 
 example7.Dog = {}
-example7.Dog.maxSound = () => alert('Wuff')
-example7.Dog.bellaSound = () => alert('Wau')
+example7.Dog.maxSound = (name) => alert('Wuff, I am ' + name)
+example7.Dog.bellaSound = (name) => alert('Wau, I am ' + name)
 
-example7.registerComponent(Dog, 'dog1', [], 'Max', "example7.Dog.maxSound()")
-example7.registerComponent(Dog, 'dog2', [], 'Bella', "example7.Dog.bellaSound()")
+example7.registerComponent(Dog, 'dog1', [], 'Max', "example7.Dog.maxSound")
+example7.registerComponent(Dog, 'dog2', [], 'Bella', "example7.Dog.bellaSound")
 
 example7.initialRender(Dogshouse)
--- getstarted.js
+++ getstarted.js
@@ -1,23 +1,28 @@
-import { RobJSApp } from "./Rob.js";
+import { RobJSApp } from './Rob.js'
 
 const getstarted = new RobJSApp('getstarted')
 getstarted.init('getstarted')
 
-getstarted.defineStateVar('count', 0);
+getstarted.defineStateVar('count', 0)
 
-const Counter = () => {
-
-    console.log(getstarted.oldState.count)
-
+const Counter = (id) => {
     return `
-      <div class="Counter"> 
-        <button type="button" class="btn btn-danger" onclick="getstarted.updateState('count', getstarted.state.count - 1)">-</button>
+      <div id='${id}'> 
+        <button type='button' class='btn btn-danger' onclick='getstarted.updateState("count", getstarted.state.count - 1)'>-</button>
         <span class="text-dark">${getstarted.state.count}</span>
-        <button type="button" class="btn btn-success" onclick="getstarted.updateState('count', getstarted.state.count + 1)">+</button>
+        <button type='button' class='btn btn-success' onclick='getstarted.updateState("count", getstarted.state.count + 1)'>+</button>
       </div>
     `
 }
 
-getstarted.registerComponent(Counter, 'Counter', ['count'])
+getstarted.registerComponent(Counter, 'counter1', ['count'])
 
-getstarted.render(Counter) 
+const App = () => {
+  return `
+    <div> 
+      ${Counter('counter1')}
+    </div>
+  `
+}
+
+getstarted.initialRender(App) 
